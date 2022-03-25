3a7cc78adfc9fde54beed5d0313eabdf1eed33f7
kchen141 <84304876+kchen141@users.noreply.github.com>
2022-03-24 22:24:10 -0400

    finished

---
+++ myapp/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import './Horoscope';
+import Horoscope from './Horoscope';
+
+function App() {
+  return (
+      <div className="App">
+        <Horoscope />
+      </div>
+  );
+}
+
+export default App;
